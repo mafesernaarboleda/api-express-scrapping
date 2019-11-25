4cf3341a779d7662981e68bc7a21cdbaf4a4f97b
Mafe Serna <mafesernaarboleda@gmail.com>
2019-11-24 21:30:32 -0500

    fix name slide

--- src/scrap.service.js
+++ src/scrap.service.js
@@ -3,37 +3,47 @@
  * Main application routes
  */
 
-const rp = require('request-promise');
-const cheerio = require('cheerio');
+const rp = require("request-promise");
+const cheerio = require("cheerio");
 
 function find(name, res) {
-    const options = {
-        uri: `https://slides.com/${name}`,
-        transform: (body) => {
-            return cheerio.load(body);
-        }
-    };
-    let presentations = []
-    return new Promise((success, reject) => {
-        rp(options)
-            .then(($) => {
-                $('li').each((i, elem) => {
-                    const imageStyle = $(elem).find($('.deck-image')).attr('style')
-                    const imageData = $(elem).find($('.deck-image')).data()
-                    let pre = {
-                        ...$(elem).data().id && $(elem).data(),
-                        imageUrl: (imageStyle && imageStyle.match(/url\(["']?([^"']*)["']?\)/)[1]) || (imageData && imageData.imageUrl)
-                    }
-                    presentations.push(pre)
-                });
-                success(presentations.filter(pre => pre.id))
-            })
-            .catch((err) => {
-                reject(err);
-            });
-    });
+  const options = {
+    uri: `https://slides.com/${name}`,
+    transform: body => {
+      return cheerio.load(body);
+    }
+  };
+  let presentations = [];
+  return new Promise((success, reject) => {
+    rp(options)
+      .then($ => {
+        $("li").each((i, elem) => {
+          const imageStyle = $(elem)
+            .find($(".deck-image"))
+            .attr("style");
+          const imageData = $(elem)
+            .find($(".deck-image"))
+            .data();
+          let pre = {
+            ...($(elem).data().id && $(elem).data()),
+            imageUrl:
+              (imageStyle &&
+                imageStyle.match(/url\(["']?([^"']*)["']?\)/)[1]) ||
+              (imageData && imageData.imageUrl),
+            name: $(elem)
+              .find($(".deck-title-value"))
+              .text()
+          };
+          presentations.push(pre);
+        });
+        success(presentations.filter(pre => pre.id));
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
 
 module.exports = {
-    find
+  find
 };
